43d71e78e745221e21b89f52b44125c2dcc6c9b5
Y. Z. Chen <754097987@qq.com>
2022-04-28 23:00:41 +0800

    [Update] change directory separator direction.

--- Extract_Scripts/src/main/kotlin/com/geno1024/qqtangextractor/ds/ResourceTree.kt
+++ Extract_Scripts/src/main/kotlin/com/geno1024/qqtangextractor/ds/ResourceTree.kt
@@ -6,7 +6,7 @@ import java.io.File
 object ResourceTree
 {
     fun listFiles(file: File, tabDepth: Int = 0): String =
-        "${"    ".repeat(tabDepth)}- [${if (File("${Settings.version}/${file.relativeTo(File(Settings.base))}").absoluteFile.parentFile.takeIf { it.exists() }?.listFiles { it -> it.nameWithoutExtension == file.nameWithoutExtension }?.isNotEmpty() == true) "X" else " "}] ${file.relativeTo(File(Settings.base))}\n${if (file.isDirectory) file.listFiles().joinToString(separator = "") { listFiles(it, tabDepth + 1) } else ""}"
+        "${"    ".repeat(tabDepth)}- [${if (File("${Settings.version}/${file.relativeTo(File(Settings.base))}").absoluteFile.parentFile.takeIf { it.exists() }?.listFiles { it -> it.nameWithoutExtension == file.nameWithoutExtension }?.isNotEmpty() == true) "X" else " "}] ${file.toRelativeString(File(Settings.base)).replace("\\", "/")}\n${if (file.isDirectory) file.listFiles().joinToString(separator = "") { listFiles(it, tabDepth + 1) } else ""}"
 
     fun toTreeDiagram() = listFiles(File(Settings.base))
 }
